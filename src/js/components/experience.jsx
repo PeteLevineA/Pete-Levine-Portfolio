231fd3822b9f2a8e049d7d9e14b1ea488d863755
PeteLevineA <pete@ionisix.com>
2016-01-18 00:33:27 -0500

    Better Experience

--- src/js/components/experience.jsx
+++ src/js/components/experience.jsx
@@ -14,20 +14,22 @@ var Experience = React.createClass({
                 </Planet>
                 <Planet title="Caterpillar" jobTitle="Senior Software Engineer" subTitle="Feb. 15 - Current" iconImg="images/mars.svg">
                     - Front end development for assembly line applications<br />
-                    - Tech: Knockoutjs, Bootstrap, jQuery, HTML5 & CSS3<br />
+                    - Worked with product teams to mock-up, design, data-model and develop responsive web applications using Knockout JS, Bootstrap, jQuery, HTML5, & CSS3<br />
                     - Code reviewer, mentor, helped develop the team's software process
                 </Planet>
                 <Planet title="Innovatient" jobTitle="Senior Software Developer" subTitle="Sep. 13 - Feb. 15" iconImg="images/jupiter.svg">
                     - Front end development for patient engagement applications<br />
-                    - Front end: HTML5 & CSS3 Responsive Apps, jQuery, Light AngularJS<br />
-                    - Back end: .Net MVC RESTful Clinical App Integration, C#, SQL<br />
+                    - Created the HTML5 and CSS3 responsive patient facing mobile application using jQuery, Razor, C# and Light AngularJS<br />
+                    - Clinical software integration using MVC APIs<br />
+                    - Travelled to customer sites for software installation, go lives and software updates<br />
                     - Wore many hats in a less than ten person startup
                 </Planet>
                 <Planet title="Allscripts" jobTitle="Software Engineer to Senior Software Engineer" subTitle="Feb. 08 - Sep. 13" iconImg="images/saturn.svg">
-                    - Front end development for hospital healthcare<br />
-                    - Front end: jQuery, Telerik, VanillaJS
-                    - Back end: C#, SQL Server<br />
-                    - Trained others on Front-End dev. Ran Lunch and Learns
+                    - Front end development for hospital healthcare in jQuery, Telerik and Vanilla JavaScript<br />
+                    - Designed and developed with Product, UI and Business team critical user interface elements and controls<br />
+                    - Trained others on Front-End dev. Ran Lunch and Learns<br />
+                    -  C# and SQL Server development in the backend for front-end integration<br />
+                    - Scrum Master. Development in an Agile SDLC with Scrum and Scrum Master Training
                 </Planet>
                 <Planet title="Moving Pictures" jobTitle="Web Developer" subTitle="Feb. 07 to Jan. 08" iconImg="images/neptune.svg">
                     - Front end development for healthcare marketing<br />
